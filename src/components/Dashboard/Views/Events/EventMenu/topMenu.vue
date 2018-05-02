29d23045edb9257c8cfbf9301fdb68c7045c84f3
ghovat <georg@sattler.io>
2018-05-02 15:40:34 +0200

    create product top menu

--- src/components/Dashboard/Views/Events/EventMenu/topMenu.vue
+++ src/components/Dashboard/Views/Events/EventMenu/topMenu.vue
@@ -1,12 +1,19 @@
 <template>
   <div class="card">
-    <div class="card-content text-center center">
-      <div class="btn-group">
-        <button type="button" class="btn btn-default">Left</button>
-        <button type="button" class="btn btn-default">Middle</button>
-        <button type="button" class="btn btn-default">Right</button>
+    <nav class="navbar navbar-default">
+      <div class="container-fluid">
+        <div class="navbar-absolute">
+          <ul class="nav navbar-nav navbar-left">
+            <li class="open" v-bind:class="$route.name == 'Event details' ? 'active isActive is-active active-class' : ''">
+              <router-link to="/admin/stats" class="dropdown-toggle btn-magnify" data-toggle="dropdown">
+                <i class="ti-home"></i>
+                <p>Overview</p>
+              </router-link>
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
+    </nav>
   </div>
 </template>
 <script>
@@ -15,6 +22,26 @@
     props: {
     },
     computed: {
+    },
+    methods: {
+      capitalizeFirstLetter (string) {
+        return string.charAt(0).toUpperCase() + string.slice(1)
+      },
+      toggleNotificationDropDown () {
+        this.activeNotifications = !this.activeNotifications
+      },
+      closeDropDown () {
+        this.activeNotifications = false
+      },
+      toggleSidebar () {
+        this.$sidebar.displaySidebar(!this.$sidebar.showSidebar)
+      },
+      hideSidebar () {
+        this.$sidebar.displaySidebar(false)
+      },
+      minimizeSidebar () {
+        this.$sidebar.toggleMinimize()
+      }
     },
     data () {
       return {
